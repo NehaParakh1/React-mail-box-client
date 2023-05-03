288e752d10e3c0b6412c4de24908dd24def39a4c
NehaParakh1 <nehadugar3@gmail.com>
2023-05-03 16:22:43 +0530

    03_Login screen

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,12 @@
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={classes.header}>Welcome to Mail Box</header>
+
+    )
+}
+
+
+
+export default Header
